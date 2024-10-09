bbca214a43a44aec0e425d2d7c007af5bcb9d0b1
henkspie <henkspi@me.com>
2024-10-09 15:19:21 +0200

    Refractor config/settings /base.py and manage.py.

--- app/manage.py
+++ app/manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svd.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +16,13 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # This allows easy placement of apps within the interior
+    # svd directory.
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "svd"))
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
