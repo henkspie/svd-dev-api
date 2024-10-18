c0c23a42de7c7929dcf35334e3d2ae69c36798a3
henkspie <henkspi@me.com>
2024-10-18 14:22:54 +0200

    Normalisation svdUser.

--- app/svd/core/models.py
+++ app/svd/core/models.py
@@ -2,6 +2,7 @@
 Database models
 
 """
+import datetime
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -11,12 +12,29 @@ from django.contrib.auth.models import (
 from django.utils.translation import gettext_lazy as _
 
 
+def check_normalize_svdUser(name):
+    """Check the svdUser name construct is correct."""
+    txt = name.split("_")
+    if len(txt[1]) == 8 and txt[1].isnumeric():
+        # we do not allow users older as 120 years or younger as 5
+        year = int(txt[1][:4])
+        now = int(datetime.datetime.now().year)
+        if year not in range(now-5, now-120):
+            return False
+
+        return f"{txt[0].capitalize()}_{txt[1]}"
+    return False
+
+
 class SvdUserManager(BaseUserManager):
     """Manager fo SvdUsers."""
 
-    def create_user(self, svdUser, password=None, **extra_fields):
+    def create_user(self, svdUser, password=None, email=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(svdUser=svdUser, email=None, **extra_fields)
+        svdUser = check_normalize_svdUser(svdUser)
+        if not svdUser:
+            raise ValueError(_("Not a correct SvdUser name given."))
+        user = self.model(svdUser=svdUser, email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
--- app/svd/core/tests/test_models.py
+++ app/svd/core/tests/test_models.py
@@ -19,3 +19,37 @@ class model_tests(TestCase):
 
         self.assertEqual(user.svdUser, username)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_svdUser_normalized(self):
+        """ Test the svdUser is normalized for new user."""
+
+        sample_svdUsers = [
+             ["User_19500601", "User_19500601"],
+             ["UsEr_19500602", "User_19500602"],
+             ["User-Peter_19500601", "User-peter_19500601"],
+             ["USEr-peter_19500602", "User-peter_19500602"],
+        ]
+        for svdUser, expected in sample_svdUsers:
+            user = get_user_model().objects.create_user(svdUser, "testpass123")
+            self.assertEqual(user.svdUser, expected)
+
+    def test_wrong_format_new_user_raises_error(self):
+        """Test wrong format raises error. """
+        wrong_users = [
+            "User_1950106",
+            "User_195010106",
+            "User_18500106",
+        ]
+        for svdUser in wrong_users:
+            with self.assertRaises(ValueError):
+                user = get_user_model().objects.create_user(svdUser, "testpass123") # noqa
+
+    # def test_svdUser_is_in_familyTree(self):
+    #     """Test the svUser is a member of the family tree."""
+    #     firstname = "Jan Karel"
+    #     name = "User"
+    #     birthday = "1959-01-06"
+    #     member = FamilyTree.objects.create(firstname=firstname,
+    #                                        name=name, birthday=birthday)
+
+    #     username = "User_19500106"
