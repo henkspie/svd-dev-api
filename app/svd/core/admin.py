1ec821893675e9a3b52ef538cde85ea9cc2fc17b
henkspie <henkspi@me.com>
2024-10-20 08:07:16 +0200

    Setup Django Admin.

--- app/svd/core/admin.py
+++ app/svd/core/admin.py
@@ -1,3 +1,46 @@
-from django.contrib import admin    # noqa: F401
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """ Define the admin pages for users."""
+    ordering = ["id"]
+    list_display = ["svdUser", "email"]
+    fieldsets = (
+        (None, {'fields': ('svdUser', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', )}),
+    )
+    readonly_fields = ['last_login', 'svdUser']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'svdUser',
+                'email',
+                'password1',
+                'password2',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+
+admin.site.register(models.SvdUser, UserAdmin)
---
+++ app/svd/core/tests/test_admin.py
@@ -0,0 +1,47 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """ Django admin tests."""
+
+    def setUp(self):
+        """ Create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            svdUser="Admin_19500106",
+            password="testpass123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            svdUser="User_19600106",
+            password="testpass123",
+            email="user@example.com"
+        )
+
+    def test_users_list(self):
+        """ Test that users are listed on page."""
+        url = reverse("admin:core_svduser_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.svdUser)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test the edit svdUser page works."""
+        url = reverse("admin:core_svduser_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """ Test the create svdUser page works."""
+        url = reverse("admin:core_svduser_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
